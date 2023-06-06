f09e255a9a2c7ab8395cf2e05deba3aa8297159e
Florian Isopp <florian.johannes.isopp@gmail.com>
2023-06-06 15:47:40 +0200

    added boilerplate

---
+++ uploadResultsToTestrail_mod_boiler.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+
+def parse_xml_file(file_path):
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        
+        # Access elements and perform further processing
+        # Example: Print the tag and text of each element
+        for element in root.iter():
+            print("Tag:", element.tag)
+            print("Text:", element.text)
+
+    except ET.ParseError as e:
+        print(f"Error parsing XML file: {str(e)}")
+
+# Provide the path to your XML file here
+xml_file_path = "path/to/your/xml/file.xml"
+parse_xml_file(xml_file_path)
